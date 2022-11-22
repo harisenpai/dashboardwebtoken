62677d00a6ec65a3a9ddb32b0d208900ee5d6a86
kunal.p <kunal.p@gametion.com>
2022-11-22 12:07:43 +0530

    error handling with status codes

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-function isWds(string) {
-  console.log("herehrehr");
-  return string === "WDS"
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
+module.exports = Verifier;
+function Verifier(options) {
+    this.options = options || {};
 }
 
-let token = require('./token.js');
+Verifier.prototype.verify = async function (body) {
+    try {
+        const token = body;
+        if (!token) {
+            const err = new Error('Token is required');
+            err.data = 'token is required';
+            err.status = 412;
+            throw err;
+        }
 
-module.exports =
-{
-  isWds,
-  token
+        const secret = this.options.secret;
+        if (!secret) {
+            const err = new Error('Secret is required');
+            err.data = 'Secret is required';
+            err.status = 412;
+            throw err;
+        }
+
+        const decoded = await _fnDecodeToken(token, secret);
+        const _adminId = decoded._id || null;
+        if (!_adminId)  {
+            const err = new Error('Invalid admin');
+            err.data = 'Invalid admin';
+            err.status = 401;
+            throw err;
+        }
+
+        const url = `https://json.ludokingapi.com/api/v2/auth/token/${_adminId}`;
+        const adminInfo = await axios.get(url);
+        if (!adminInfo.data || adminInfo.data.token != token)  {
+            const err = new Error('token expired');
+            err.data = 'token expired';
+            err.status = 401;
+            throw err;
+        }
+
+        return (
+            {
+                isSuccess: 1,
+                "_adminId": _adminId,
+                "permissions": JSON.parse(adminInfo.data.permissions)
+            }
+        );
+    }
+    catch (err) {
+        throw err
+    }
 }
+
+const _fnDecodeToken = async (token, secret) => {
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        const tokenError = new Error('Authentication Error');
+        tokenError.data = 'authentication_error';
+        tokenError.status = 401;
+        throw tokenError;
+    }
+};
 
